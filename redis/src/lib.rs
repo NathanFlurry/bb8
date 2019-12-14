b239dff1ac2129bf9aad3fbc11c1f8667dd0dd51
Nathan Flurry <developer@nathanflurry.com>
2019-12-14 18:54:15 +0000

    Add clone to `RedisPool`

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -18,7 +18,7 @@ type Result<T> = std::result::Result<T, RedisError>;
 
 /// `RedisPool` is a convenience wrapper around `bb8::Pool` that hides the fact that
 /// `RedisConnectionManager` uses an `Option<Connection>` to smooth over the API incompatibility.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct RedisPool {
     pool: bb8::Pool<RedisConnectionManager>,
 }
